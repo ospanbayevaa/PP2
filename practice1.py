49124c0f8180be468fdc7e6540105977dd23c613
Adina Ospanbayeva <a_ospanbayeva@kbtu.kz>
2025-01-15 23:01:51 +0500

    Completed

--- practice1.py
+++ practice1.py
@@ -1,1 +1,81 @@
+#HOME
 print("Hello, world!")
+
+#Syntax
+if 5 > 2:
+    print("Five is grater than two!")
+
+#Comments
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#Data types
+x = 5
+print(type(x))
+
+#Numbers
+x = 1    
+y = 2.8  
+z = 1j   
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Casting
+x = int(1)   
+y = int(2.8) 
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+#string
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+txt1 = "We are the so-called \"Vikings\" from the north."
+print(txt1)
+
+
