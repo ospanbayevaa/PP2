beac5d8f041735d50751d6fc5b66cf24d52ec4ff
Adina Ospanbayeva <a_ospanbayeva@kbtu.kz>
2025-01-29 21:20:45 +0500

    comment

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import grams_to_ounces, fahrenheit_to_celsius, filter_prime, sphere_volume
+
+print("10 grams in ounces:", grams_to_ounces(10))
+print("100°F in Celsius:", fahrenheit_to_celsius(100))
+print("Простые числа из списка:", filter_prime([1, 3, 4, 5, 7, 8, 9, 11]))
+print("Объем сферы радиусом 3:", sphere_volume(3))
